88b27ed41f464191995ffad1ccdae22a267c4d0c
Joe Chang <joe.chang.1996@hotmail.com>
2023-02-07 20:00:15 -0600

    making changes

---
+++ src/Components/home/Data.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const 
+ = () => {
+  return (
+    <div className="home_data">
+        <h1 className="home_title">
+            Joesang Chang
+        </h1>
+        <h3 className="home_subtitle">Designer</h3>
+        <p className="home_description">I'm creative designer based in Costa Rica.</p>
+    </div>
+  )
+}
+
+export default Data
--- src/Components/home/Hom.tsx
+++ src/Components/home/Hom.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import "./home.css"
 import Social from './Social'
+import Data from './Data'
 
 export const Hom = () => {
   return (
     <section className="home section" id="home">
-    <div className="home_container container grid">
-     <div className="home_content grid">
-      Hola
+      <div className="home_container container grid">
+        <div className="home_content grid">
+           <Social/>
+           <div className="home_img"></div>
+           <Data/>
+        </div>
       </div>
-    </div>
     </section>
   )
 }
